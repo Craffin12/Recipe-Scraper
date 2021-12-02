98cb3f1fa42e18c181384ce965e48d1e186da8a4
craffin12 <raffincl@oregonstate.edu>
2021-12-02 15:09:15 -0500

    Added a requirements.txt. Additionally, refactored according to code smells

--- recipeScraper.py
+++ recipeScraper.py
@@ -7,42 +7,32 @@ app = Flask(__name__)
 
 CORS(app)  # Prevents the raising of restrictions due to CORS
 
-
-def grabRecipe(html):
+def grabRecipeLinks(html):
+    """Finds three recipe links on html Page from Allrecipes returns 
+    them in a dictionary with the title of the recipe as the key"""
     links = html.find_all('a', class_='card__titleLink manual-link-behavior')
-
     linkColl = {}
-
-    count = 0
-
     for a in links:
-        recipeTitle = a['title']
-        recipeUrl = a['href']
-        linkColl[recipeTitle] = recipeUrl
-
+        if len(linkColl) < 3:
+            recipeTitle = a['title']
+            recipeUrl = a['href']
+            linkColl[recipeTitle] = recipeUrl
     return linkColl
 
-
 def scrapeRecipe(ingredientName):
-    """
-    """
+    """Takes an ingredient name and returns a dictionary with three 
+    recipe links scrpaed from Allrecipes"""
     ingredientName = ingredientName.replace(" ", "+")
     url = 'https://www.allrecipes.com/search/results/?search=' + ingredientName
-
-    res = requests.get(url)
-
-    html = BeautifulSoup(res.content, "html.parser")
-
-    linkDict = grabRecipe(html)
-
+    resp = requests.get(url)
+    htmlDocument = BeautifulSoup(resp.content, "html.parser")
+    linkDict = grabRecipeLinks(htmlDocument)
     return linkDict
 
-
-@app.route('/recipeScraper/<string:ingName>', methods=['Get'])
-def wikiScraper(ingName: str):
-    resp = scrapeRecipe(ingName)
+@app.route('/recipeScraper/<string:ingredientName>', methods=['Get'])
+def wikiScraper(ingredientName: str):
+    resp = scrapeRecipe(ingredientName)
     return jsonify(resp)
-
 
 if __name__ == '__main__':
     app.run(port=3033)
